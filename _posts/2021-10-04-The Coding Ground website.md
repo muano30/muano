a8bf34fec60690db2bacca65f286781cc4e13763
muano30 <muanonevhufumba@gmail.com>
2021-11-22 12:01:50 +0200

    The Coding Ground Website

---
+++ _posts/2021-10-04-The Coding Ground website.md
@@ -0,0 +1,28 @@
+---
+Layout: 
+Title: "The Coding Ground Website."
+date: "2021-10-18"
+---
+
+# Introduction.
+
+We were given a project to do which is to make changes in the current TCG(The coding Ground) website, we were ask to add navigatin bar so that a user will be able to navigate TCG website. On the navigation bar we were ask to add achors tag for home, about,student, mentors and contact page.
+
+In this blog post i will share the things that i have learned, enjoy while doing project and the challenges that i faced during the process.
+
+# Body.
+
+## Explanation
+
+When i started this project i was excited because we were told to make changes using HTML and CSS which i have an understandng already. I was not doing this project alone but we were doing it as a group, meaning we have to sit down and discuss how we going to start the project,
+Our first meeting we discuss on the strategy of how we going to making changes on the website, which was a success because we agreed on who will be responsible for doing what, and that we should have a daily standup to update each other on the progress.
+
+I was responsible for doing navigation bar, about page, and contact page. what i will say is that before you can start building the website it is very important to design the wireframe first so that you will have a direction on which path you have to take, it also helps knowing what font size, font family and allignment of things etc while building the app.
+
+I enjoyed doing the about page and contact page not the navigation bar because navigation bar have to be in each and every single page of the website meaning more work.
+
+What i have learned is that working as a group is only possible if you have an agreement and both of you follow the same process that you agreed on, but the best thing in working as group is communication, i learned that as a group if you don't have good communication it also affect how you work as a group, i learn that working as as group means doing things collectively, and when it comes to present the work we have to choose one person to represent all of us, but we have to present what we have done to each other to have an understanding why they choose the approach they have choose so that we both have the same understading or hepl each other if there's a better approach.
+
+# Conclusion.
+
+The project is not finished yet we are waiting for the approval to know if we have to add anything or keep it the way it is and start working on the responsive. When the website was build we targeted the desktop screen meaning the app will not respond if someone try to open it using their phone.
--- script.js
+++ script.js
@@ -385,7 +385,6 @@
 // console.log(getHighest(numbers));
 
 
-
 // const getTotal1 = (array , name) => {
 //     var sum = 0
 //     for(var i in array){
@@ -441,12 +440,405 @@
 
 
 
-function simpleMultiplication(number) {
-   if(number % 2 == 0){
-     return number * 8
-   }else{
-      return number * 9
-   }
-}
+// function simpleMultiplication(number) {
+//    if(number % 2 == 0){
+//      return number * 8
+//    }else{
+//       return number * 9
+//    }
+// }
 
-console.log(simpleMultiplication(7))
+// console.log(simpleMultiplication(7))
+
+
+
+// function uniqueArray2(arr) {
+//    var a = [];
+//    for (var i=0, l=arr.length; i<l; i++)
+//        if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
+//            a.push(arr[i]);
+//    return a;
+// }
+
+
+// const names = [
+//    { name: "Muano", surname: "Nevhufumba", marks: 100 },
+//    { name: "Kamva", surname: "Zitho", marks: 60 },
+//    { name: "Tumi", Nevhufumba: "Ramolo", marks: 70 },
+//    { name: "Wonagni", surname: "Sikhosana", marks: 10 },
+// ]
+
+// const passedStudent = (list) => {
+//    let newlist = []
+//    for(var i of list){
+//       if(i.marks >= 50){
+//          newList.push(i.marks)
+//       }
+//    }
+//    return newList
+// }
+
+// const passedStudent = (list) => {
+//    let newList = []
+//    for(var i in list){
+//       if(list[i].marks >= 50){
+//          newList.push(list[i].marks)
+//       }
+//    }
+//    return newList
+// }
+
+// console.log(passedStudent(names))
+
+
+
+// const names = ["Thato", "Thato", "Sbu", "Sbu", "Sbu"]
+
+// const getTotalCount = (array , name) => {
+//     var sum = 0
+//     for(var i in array){
+//         if(array[i] === name){
+//             sum  = sum + 1
+//         }
+//     }
+//     return sum
+// }
+// console.log(getTotalCount(names,"Sbu"))
+
+
+// const names = ["That", "Sbu", "Sb", "muano", "Tumisang"]
+
+// const getLongestString = (array) => {
+
+//     var longestString = "";
+
+// for (var i in array) {
+//     if (array[i].length > longestString.length) {
+//         longestString = array[i];
+//     }
+// }
+// return longestString
+// }
+// console.log(getLongestString(names))
+
+// const names = ["That", "Sbu", "Sb", "muano", "Tumisang"]
+// const names1 = ["That", "Sbu", "Sb", "muano"]
+
+// const getLongestString = (array) =>{
+//     var longestWord = "";
+
+//     array.forEach(function(word) {
+//         if (word.length > longestWord.length) {
+//             longestWord = word;
+//         }
+//     });
+
+//     return longestWord;
+// }
+
+// console.log(getLongestString(names))
+// console.log(getLongestString(names1))
+
+// program to generate a multiplication table
+
+// take input from the user
+
+
+// students = [
+//    { name: "Muano", marks: 20 },
+//    { name: "Tumi", marks: 30 },
+//    { name: "Kamva", marks: 40 }
+// ]
+
+// getStudentBelowAvarage = (array) => {
+//    newMarks = [];
+//    getStudentBelowAvarage = [];
+//    for (i of array) {
+//       newMarks.push({ name: i.name, marks: i.marks * 2 })
+//    }
+//    for (i of newMarks) {
+//       if (i.marks < 50) {
+//          getStudentBelowAvarage.push(i)
+//       }
+//    }
+//    return getStudentBelowAvarage
+// }
+// console.log(getStudentBelowAvarage(students))
+
+
+// getStudentBelowAvarage = (array) => {
+//    newMark = [];
+//    getStudentBelowAvarage = [];
+//    for (i in array) {
+//       newMark.push({ name: array[i].name, marks: array[i].marks * 2 })
+//    }
+
+//    for (i in newMark) {
+//       if (newMark[i].marks < 50) {
+//          getStudentBelowAvarage.push(newMark[i])
+//       }
+//    }
+//    return getStudentBelowAvarage
+// }
+// console.log(getStudentBelowAvarage(students))
+
+
+// const numbers = [2, 2, 4, 6, 10, 10]
+
+// const removeDuplicats = (array) => {
+//    const newArray = []
+//       for(var i= 0; i < array.length; i++){
+//       if(newArray.indexOf(array[i]) === -1){
+//             newArray.push(array[i])
+//          }
+//    }
+
+//    return newArray
+// }
+// console.log(removeDuplicats(numbers))
+
+
+
+// const numbers = [2, 2, 4, 6, 10, 10]
+
+// const removeDuplicats = (array) => {
+//    const newArray = [];
+//    for (var i in array) {
+//       if (newArray.indexOf(array[i]) === -1) {
+//          newArray.push(array[i])
+//       }
+//    }
+//    return newArray
+// }
+
+// console.log(removeDuplicats(numbers))
+
+
+
+// const marks = [2, 4, 6, 7, 8]
+
+// const getAvarage = (array) => {
+//     sum = 0
+//       for(var i in array){
+//       sum = sum + array[i]/array.length
+//       }
+//      return sum/10 * 100
+//    }
+
+// console.log(getAvarage(marks))
+
+// // ////////////////////////////////add only positive numbers
+
+// const numbers = [1, 2, -3, -4, -5, ]
+
+// function positiveSum(arr) {
+//     let sum = 0;
+//     for(var i in arr){
+//     //   console.log(arr[i])
+//       if(arr[i] >= 0  ){
+//         sum += arr[i]
+//       }
+//     }
+//     return sum
+//   }
+
+//   console.log(positiveSum(numbers))
+
+// // getAverage with objects.
+
+//   const marks = [{ name: "Thabo", mark: 15 }, { name: "Muano", mark: 15 }, { name: "Angel", mark: 15}];
+
+// const getAverage = (array, totalMark) => {
+//     var sum = {mark: 0}
+//     for (var i in array) {
+//         sum.mark = sum.mark +  array[i].mark
+//     }
+
+//     var value = sum.mark / array.length
+
+//     var sumValue = value / totalMark 
+//     return sumValue * 100
+// }
+// console.log(getAverage(marks , 20))
+
+// const marks = [{ name: "Thabo", mark: 6 }, { name: "Muano", mark: 6 }, { name: "Angel", mark: 15 }];
+
+// const getAverage = (array, totalMark) => {
+//    var sum = 0;
+//    for (var i in array) {
+//       sum = sum + array[i].mark
+//    }
+
+//    var value = sum / array.length
+
+//    var sumValue = value / totalMark
+//    return sumValue * 100
+// }
+// // console.log(getAverage(marks , 20))
+
+
+// getStudentAboveAverage = (array, totalMark) => {
+//    var averageStudent = getAverage(array, totalMark)
+//    studentABoveAverage = [];
+//    for (var i in array) {
+
+
+//       if (getAverage([array[i]], totalMark) > averageStudent) {
+//          studentABoveAverage.push(array[i])
+//       }
+//    }
+//    return studentABoveAverage
+
+// }
+
+// console.log(getStudentAboveAverage(marks, 20))
+
+
+
+
+// const numbers = [2, 2, 4, 6, 10, 10, 10]
+
+// const removeDuplicats = (array) => {
+//    const newArray = [];
+//    for (var i in array) {
+//       if (newArray.indexOf(array[i]) === -1) {
+//          newArray.push(array[i])
+//       }
+//    }
+//    return newArray
+// }
+
+// console.log(removeDuplicats(numbers))
+
+
+
+// var a = 0; 
+// var b = 40;
+// function remainder(a, b){
+   // Divide the larger argument by the smaller argument and return the remainder
+   
+//    if(a > b && b === 0){
+//      return NaN;
+//    } else if(b > a && a === 0){
+//      return NaN;
+//    }
+//    return a > b ? a % b : b % a
+//  }
+
+//  console.log(remainder(a, b))
+
+
+// numberSet = -1
+
+// function opposite(number) {
+//    return - number
+//  }
+
+//  console.log(opposite(numberSet))
+
+
+
+
+
+// const values = [ 2, 4 , 6, "7", "b"]
+
+// function halfAndReturnTotal(array) {
+//   const arrayWithNoStrings = [];
+//   const arrayWithOnlyPostiveNumbers = [];
+//   const dividedList = [];
+//   var sum = 0;
+
+//   for (var i in array) {
+//     if (array[i] < 0) {
+//       arrayWithOnlyPostiveNumbers.push(array[i] * -1);
+//     }
+//     if (array[i] > 0) {
+//       arrayWithOnlyPostiveNumbers.push(array[i]);
+//     }
+//   }
+
+//   for (var i in arrayWithOnlyPostiveNumbers) {
+//     if (typeof arrayWithOnlyPostiveNumbers[i] !== "string") {
+//       arrayWithNoStrings.push(arrayWithOnlyPostiveNumbers[i]);
+//     }
+//   }
+
+//   for (var i in arrayWithNoStrings) {
+//     dividedList.push(arrayWithNoStrings[i] / 2);
+//   }
+
+//   for (var i in dividedList) {
+//     sum += dividedList[i];
+//   }
+
+//   return sum;
+// }
+
+// console.log(halfAndReturnTotal(values));
+
+
+// Write a function, gooseFilter / goose-filter / goose_filter / GooseFilter, that takes an array of strings as an argument and returns a filtered array containing the same elements but with the 'geese' removed.
+
+
+
+// let birds = ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+// function gooseFilter (birds) {
+//    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+//    return birds.filter(birds => geese.indexOf(birds) < 0)
+   
+//  };
+
+//  console.log(gooseFilter(birds))
+
+
+
+// return the lastfour values
+
+//  const numbers = [ 2, 4, 6, 8 , 10 , 2, 4, 6, 82 , 310]
+ 
+//  const returnLastFourItem = (array) => {
+//     for(var i in array){
+//        return array.slice(-4)
+//     }
+//  }
+
+// console.log(returnLastFourItem(numbers))
+
+
+
+// calculate average if it's an empty array return zero
+
+// var numbers = [10, 20, 30, 40];
+
+// function find_average(array) {
+//  sum = 0;
+//   for(var i in array){
+//   sum = sum + array[i]
+//   }  
+//   return sum/array.length || 0;
+  
+// }
+
+// console.log(find_average(numbers))
+
+
+
+// function get the ratings
+
+
+ 
+// 8 kyu Transportation on vacation
+
+// const rentalCarsCost = (d) => {
+//   let dayCost = 40;
+// if(d >= 7){
+//    return d * dayCost - 50
+// } if(d >= 3){
+//       return d * dayCost - 20 
+//    } 
+//    return d * dayCost
+// }
+
+// console.log(rentalCarsCost(6))
+
